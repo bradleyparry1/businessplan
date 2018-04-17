0ee508da5bf645e1bb7eab074c5a2ff7dedf9ae0
bradleyparry1 <bradley.parry@digital.cabinet-office.gov.uk>
2018-04-17 19:52:28 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ def authorize
     #puts url
     #code = gets
     code = "4/AACtAxlWR5dskVebdktncNGpTkrz3y9_dGrwc9aZrBhe_VDg-9YLWwU"
-    code = "4/AABTf3uizJu-dAlz8Cbe9baVk7lHCE_WNzyRCMdabmy7AelWmBZOKu4"
+    code = "4/AACDhEoQK_Gof-Hlbw08ZTDigPZhPy2JAy3TFKUCZ48F_XkLU_mqF1o"
     credentials = authorizer.get_and_store_credentials_from_code(
       user_id: user_id, code: code, base_url: OOB_URI)
   end
